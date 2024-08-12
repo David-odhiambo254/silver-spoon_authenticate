86ad18b68b4b55decec68b9bafa95624e6390361
David-odhiambo254 <davidodhiambookoth3@gmail.com>
2024-08-12 10:56:20 +0300

    first commit

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: Column(
+        children: [
+          //logo
+          Icon(
+            Icons.lock,
+            size: 100,
+            ),
+
+          //welcome back ...
+
+          //username textfield
+
+          //password textfield
+
+          // forgot password?
+
+          // signin button
+
+          // or continue with
+
+          // google + apple signin buttons
+
+          // not a member ? register now
+
+        ],
+      ),
+    );
+  }
+}
